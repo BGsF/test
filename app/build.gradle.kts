ae094e6a2cdf052f92836b02a9d9dafe5b30514f
BGsF <53890907+BGsF@users.noreply.github.com>
2026-09-20 20:08:06 +0900

    Build native Value Band Scanner v1.0.3

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.wooil.valuebandscanner"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.wooil.valuebandscanner"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.0.3"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
