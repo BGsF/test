0e7648a4063a5e7b930e915a2116c17730c54f89
BGsF <53890907+BGsF@users.noreply.github.com>
2026-09-21 00:34:36 +0900

    Bump Value Band Scanner to 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.wooil.valuebandscanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
     }
 
     buildTypes {
